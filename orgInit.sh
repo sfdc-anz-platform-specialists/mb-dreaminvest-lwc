acebb608501f67d6361a36f179b87fa84a913305
Mike Burnside <mburnside@mburnside-ltm2.internal.salesforce.com>
2020-04-17 15:45:42 +1000

    orgInit.sh

---
+++ orgInit.sh
@@ -0,0 +1,6 @@
+sfdx force:org:create -s -f config/project-scratch-def.json -a dreaminvest
+sfdx force:source:push
+sfdx force:user:permset:assign -n dreaminvest
+sfdx force:data:bulk:upsert -s Sector__c -f ./data/sectors.csv -w 1 -i Sector_Id__c
+sfdx force:data:bulk:upsert -s Fund__c -f ./data/funds.csv -w 1 -i Id
+sfdx force:org:open -p /lightning/page/home
